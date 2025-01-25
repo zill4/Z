dfd6c26d1c628dc245da68f5b8c8aef441c0230d
Justin Crisp <justin@crispcode.io>
2025-01-24 21:28:05 -0600

    updated init

---
+++ Source/Z/ZGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ZGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AZGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AZGameMode();
+};
+
+
+
